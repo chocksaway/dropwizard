aa6f3c9a8eeb7d1e06c82881daf9230d04cb069c
milesd <milesd@chocksaway.com>
2019-06-25 23:22:06 +0100

    Initial import Based on https://automationrhapsody.com/build-a-rest-stub-server-with-dropwizard/

---
+++ src/main/java/com/automationrhapsody/reststub/RestStubApp.java
@@ -0,0 +1,21 @@
+package com.automationrhapsody.reststub;
+
+import com.automationrhapsody.reststub.resources.PersonService;
+import io.dropwizard.Application;
+import io.dropwizard.setup.Environment;
+
+public class RestStubApp extends Application<RestStubConfig> {
+
+    public static void main(String[] args) throws Exception {
+        new RestStubApp().run(args);
+    }
+
+    @Override
+    public void run(RestStubConfig config, Environment env) {
+        final PersonService personService = new PersonService();
+        env.jersey().register(personService);
+
+        env.healthChecks().register("template",
+                new RestStubCheck(config.getVersion()));
+    }
+}
